ec0ccc03d91fa20d29c192493f366c3871dda740
DmytroBozhor <dimaastop1@gmail.com>
2024-03-24 20:18:37 +0100

    get random detail method refactoring

--- src/main/java/dmytro/bozhor/concurrent/tasks/two/Detail.java
+++ src/main/java/dmytro/bozhor/concurrent/tasks/two/Detail.java
@@ -12,18 +12,7 @@ public enum Detail {
     HDD;
 
     public static Detail getRandomDetail() {
-        var index = RandomUtil.getRandomInt(9);
-        return switch (index) {
-            case 0 -> HEAD;
-            case 1 -> BODY;
-            case 2 -> LEFT_ARM;
-            case 3 -> RIGHT_ARM;
-            case 4 -> LEFT_LEG;
-            case 5 -> RIGHT_LEG;
-            case 6 -> CPU;
-            case 7 -> RAM;
-            case 8 -> HDD;
-            default -> throw new RuntimeException();
-        };
+        var values = Detail.values();
+        return values[RandomUtil.getRandomInt(values.length)];
     }
 }
